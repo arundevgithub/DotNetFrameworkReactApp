c3809fa66721e01d757ae85341031507e8f7e886
Arun Krishnankutty <arun.krishnankutty@gmail.com>
2024-05-19 01:29:00 -0400

    Added API call

--- Controllers/ReactController.cs
+++ Controllers/ReactController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Text.Json;
 
 namespace DotNetReact.Controllers
 {
@@ -13,5 +14,17 @@ namespace DotNetReact.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public JsonResult GetHello() 
+        {
+            Person p = new Person { Name = "Hero" };
+            return  Json(p, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class Person 
+    { 
+        public string Name { get; set; }
     }
 }
--- Scripts/reactapp.jsx
+++ Scripts/reactapp.jsx
@@ -1,13 +1,48 @@
 ﻿class ReactApp extends React.Component {
     render() {
         return (
-            <h1>My React App</h1>
+            <>
+                <h1>My React App</h1>
+                <MyComponent/>
+            </>
         )
     }
 }
 
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: "try"
+        };
+    }
 
-ReactDOM.render(
-    <ReactApp></ReactApp>,
-    document.getElementById('DivReactJS')
-)
+    render() {
+        return(
+            <>
+                <button onClick={this.callAPI}>Call API</button>
+                <label>{this.state.data}</label>
+            </>
+        )
+    }
+
+    callAPI = () => {
+        
+        fetch('/React/GetHello')
+            .then((response => response.json()))
+            .then((person) => {
+                this.setState({ data: person.Name })
+            });
+            
+        
+    }
+}
+
+//ReactDOM.render(
+//    <ReactApp></ReactApp>,
+//    document.getElementById('DivReactJS')
+//)
+
+const container = document.getElementById('DivReactJS');
+const root = ReactDOM.createRoot(container);
+root.render(<ReactApp/>);
