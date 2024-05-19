88717ba37e6b7851ebb235c58fbe4a341e0ed641
Arun Krishnankutty <arun.krishnankutty@gmail.com>
2024-05-18 22:56:29 -0400

    Workinn application, first commit

---
+++ Scripts/reactapp.jsx
@@ -0,0 +1,13 @@
+﻿class ReactApp extends React.Component {
+    render() {
+        return (
+            <h1>My React App</h1>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <ReactApp></ReactApp>,
+    document.getElementById('DivReactJS')
+)
